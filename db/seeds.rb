7e90e99460d549c3bb0487f1c92ced6f25ee8763
edouard modolo <edouardtravers@gmail.com>
2023-11-21 11:59:29 +0100

    front 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@ Underwear.destroy_all
 
 
 slip1 = {user_id: 1, title: "Slip Eminence", description: "Agreable à porter", url_photo: "https://i.ebayimg.com/images/g/1uMAAOSwj6RkLBRF/s-l1200.webp"}
-slip2 = {user_id: 1, title: "Slip Kiabi", description: "100% coton bio", url_photo: "https://i.ebayimg.com/images/g/1uMAAOSwj6RkLBRF/s-l1100.webp" }
+slip2 = {user_id: 1, title: "Slip Kiabi", description: "100% coton bio", url_photo: "https://i.ebayimg.com/images/g/1uMAAOSwj6RkLBRF/s-l1200.webp" }
 slip3 = {user_id: 1, title: "Slip Dim", description: "confortable et propre", url_photo: "https://i.ebayimg.com/images/g/1uMAAOSwj6RkLBRF/s-l1200.webp" }
 slip4 = {user_id: 1, title: "Slip H&M", description: "slip kangourou", url_photo: "https://i.ebayimg.com/images/g/1uMAAOSwj6RkLBRF/s-l1200.webp"}
 slip5 = {user_id: 1, title: "Slip Calvin Klein", description: "slip simple", url_photo: "https://i.ebayimg.com/images/g/1uMAAOSwj6RkLBRF/s-l1200.webp"}
+
+
 [slip1, slip2, slip3, slip4, slip5].each do |attributes|
   underwear = Underwear.create!(attributes)
 end
