4842d4bc4ae33a78537a803c703e85e4d6d5d534
Francois Pazery <asper-01@hotmail.fr>
2023-11-23 15:57:52 +0100

    Modifications dates

--- app/models/underwear.rb
+++ app/models/underwear.rb
@@ -4,4 +4,11 @@ class Underwear < ApplicationRecord
   has_one_attached :photo
   validates :title, presence: true
   validates :description, presence: true
+
+  # Disponibilité des Undewear
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
